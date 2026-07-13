a2bc6cd28ca800b4f92e701f1b049cf7eec489b1
ankita261152 <ankitajadhav261153@gmail.com>
2026-07-14 00:15:28 +0530

    requirements .txt file

--- Backend/app/core/database.py
+++ Backend/app/core/database.py
@@ -3,7 +3,11 @@ from sqlalchemy.orm import DeclarativeBase
 from app.core.settings import settings
 
 # Create the async engine using the database URL from settings
-engine = create_async_engine(settings.DATABASE_URL, echo=True)
+engine = create_async_engine(
+    settings.DATABASE_URL, 
+    echo=True,
+    connect_args={"statement_cache_size": 0, "prepared_statement_cache_size": 0}
+)
 
 # Create a sessionmaker bound to the engine
 AsyncSessionLocal = async_sessionmaker(
